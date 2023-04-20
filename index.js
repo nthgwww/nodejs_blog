66af97379e3cca1ba4f1cfcaa0e4af39e3405a9b
ngocthong <6051071118@st.utc2.edu.vn>
2023-04-20 22:26:36 +0700

    morgan

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
-//route no
+
+//use morgan to watch http request on terminal of VS
+var morgan = require('morgan')
+
+app.use(morgan('combined'))
+
+//route 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
